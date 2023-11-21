93d6c123ea35e7abafde3166580df94791862c46
WIN-6LUN2D0CH7K\user <tofiqmvsm2003@gmail.com>
2023-11-21 22:50:37 +0400

    Adding Map to contact page

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -3,8 +3,7 @@ import './index.scss'
 import Animatedletters from '../AnimatedLetters'
 import { useEffect, useRef, useState } from 'react'
 import emailjs from '@emailjs/browser'
-
-
+import { MapContainer, TileLayer,Marker, Popup} from 'react-leaflet'
 const Contact = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
   const refForm = useRef()
@@ -15,24 +14,24 @@ const Contact = () => {
     }, 3000)
   }, [])
 
-
-  const sendEmail = (e)=>{
+  const sendEmail = (e) => {
     e.preventDefault()
-    emailjs.sendForm(
+    emailjs
+      .sendForm(
         `${process.env.REACT_APP_SERVICE_ID}`,
         `${process.env.REACT_APP_TEMPLATE_ID}`,
         refForm.current,
-        `${process.env.REACT_APP_PUBLIC_KEY}`
-
-    ).then(
-        ()=>{
-            alert('Message successfully sent!')
-            window.location.reload(false)
+        `${process.env.REACT_APP_PUBLIC_KEY}`,
+      )
+      .then(
+        () => {
+          alert('Message successfully sent!')
+          window.location.reload(false)
         },
-        ()=>{
-            alert('Failed to send the message,please try again')
-        }
-    )
+        () => {
+          alert('Failed to send the message,please try again')
+        },
+      )
   }
 
   return (
@@ -51,28 +50,61 @@ const Contact = () => {
             established IT company with the opportunity to work with the latest
             technologies on challenging and diverse projects.
           </p>
-          <div className='contact-form'>
-          <form ref={refForm} onSubmit={sendEmail}>
-            <ul>
-                <li className='half'>
-                    <input type='text' name='from_name' placeholder='Name' required/>
+          <div className="contact-form">
+            <form ref={refForm} onSubmit={sendEmail}>
+              <ul>
+                <li className="half">
+                  <input
+                    type="text"
+                    name="from_name"
+                    placeholder="Name"
+                    required
+                  />
                 </li>
-                <li className='half'>
-                    <input type='email' name='from_email' placeholder='Email' required/>
+                <li className="half">
+                  <input
+                    type="email"
+                    name="from_email"
+                    placeholder="Email"
+                    required
+                  />
                 </li>
                 <li>
-                    <input placeholder="Subject" type="text" name="subject" required/>
+                  <input
+                    placeholder="Subject"
+                    type="text"
+                    name="subject"
+                    required
+                  />
                 </li>
-    <li>
-        <textarea required placeholder='Message' name='message'></textarea>
-    </li>
-    <li>
-        <input type='submit' className='flat-button' value="Send"/>
-    </li>
-            </ul>
-          </form>
-
+                <li>
+                  <textarea
+                    required
+                    placeholder="Message"
+                    name="message"
+                  ></textarea>
+                </li>
+                <li>
+                  <input type="submit" className="flat-button" value="Send" />
+                </li>
+              </ul>
+            </form>
           </div>
+        </div>
+        <div className='info-map'>
+        Tofiq Movsumov
+            <br/>
+            Azerbaijan
+            <br/>
+            Babak street,Sumgait
+        </div>
+        <div className='map-wrap'>
+        <MapContainer center={[40.56735813942616, 49.695225003687014]} zoom={13}>
+            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            <Marker position={[40.56735813942616, 49.695225003687014]}>
+              <Popup>Tofiq is living here,come here for a cup of coffee</Popup>
+            </Marker>
+          </MapContainer>
         </div>
       </div>
       <Loader type="pacman" />
