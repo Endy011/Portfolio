0b5540d0171757afef897958e595b1f3ba75dab1
WIN-6LUN2D0CH7K\user <tofiqmvsm2003@gmail.com>
2023-11-21 22:27:42 +0400

    Adding emailjs to get email from the people who wants to hire me

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,15 +1,39 @@
 import Loader from 'react-loaders'
 import './index.scss'
 import Animatedletters from '../AnimatedLetters'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
+import emailjs from '@emailjs/browser'
+
+
 const Contact = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
+  const refForm = useRef()
 
   useEffect(() => {
     setTimeout(() => {
       return setLetterClass('text-animate-hover')
     }, 3000)
   }, [])
+
+
+  const sendEmail = (e)=>{
+    e.preventDefault()
+    emailjs.sendForm(
+        `${process.env.REACT_APP_SERVICE_ID}`,
+        `${process.env.REACT_APP_TEMPLATE_ID}`,
+        refForm.current,
+        `${process.env.REACT_APP_PUBLIC_KEY}`
+
+    ).then(
+        ()=>{
+            alert('Message successfully sent!')
+            window.location.reload(false)
+        },
+        ()=>{
+            alert('Failed to send the message,please try again')
+        }
+    )
+  }
 
   return (
     <>
@@ -28,13 +52,13 @@ const Contact = () => {
             technologies on challenging and diverse projects.
           </p>
           <div className='contact-form'>
-          <form>
+          <form ref={refForm} onSubmit={sendEmail}>
             <ul>
                 <li className='half'>
-                    <input type='text' name='name' placeholder='Name' required/>
+                    <input type='text' name='from_name' placeholder='Name' required/>
                 </li>
                 <li className='half'>
-                    <input type='email' name='email' placeholder='Email' required/>
+                    <input type='email' name='from_email' placeholder='Email' required/>
                 </li>
                 <li>
                     <input placeholder="Subject" type="text" name="subject" required/>
